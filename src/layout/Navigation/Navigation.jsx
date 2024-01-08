293ddc7971aec0ae151f69afba5040856fce7633
zahrabagheri1 <zahrabagheriuk@gmail.com>
2024-01-08 10:14:42 +0330

    Feature: add time in navigation - Fix: add time and date and messege

--- src/layout/Navigation/Navigation.jsx
+++ src/layout/Navigation/Navigation.jsx
@@ -20,7 +20,7 @@ function Navigation() {
   const date = `${day}, ${today.getDate()} ${today.toLocaleDateString(locale, { month: 'long' })}\n\n`;
 
   const hour = today.getHours();
-  const wish = `Good ${(hour < 12 && 'Morning') || (hour < 17 && 'Afternoon') || 'Evening'}, `;
+  const wish = `Good ${(hour < 12 && 'Morning') || (hour < 17 && 'Afternoon') || (hour < 20 && 'Evening') || 'Night'}, `;
 
   const time = today.toLocaleTimeString(locale, { hour: 'numeric', hour12: true, minute: 'numeric' });
 
