e102187e26eb0b262946c8a75bd308451b7c924d
zahrabagheri1 <zahrabagheriuk@gmail.com>
2024-01-15 13:45:30 +0330

    Feature: add HiChevronDown - Fix: add HiChevronDown icon and change hover tab

--- src/Components/Tab/Tab.jsx
+++ src/Components/Tab/Tab.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useCookies } from 'react-cookie';
 import { HiChevronDown, HiRocketLaunch, HiMiniUser, HiMiniShoppingCart, HiMiniCreditCard, HiMiniArrowDownTray, HiMiniSwatch, HiCurrencyDollar, HiMiniChatBubbleLeftRight, HiOutlineArrowLeftOnRectangle, HiInboxArrowDown, HiComputerDesktop, HiChevronUp } from "react-icons/hi2";
 import { useNavigate } from 'react-router-dom';
-
+import { GoDotFill } from "react-icons/go";
 
 function Tab(props) {
     const [click, setClick] = useState(false)
@@ -44,37 +44,36 @@ function Tab(props) {
             setClick(true)
             setChild(true)
         }
-
     }
-    const clickChildHandler = ( id, link) => {
+
+    const clickChildHandler = (id, link) => {
         // console.log(link, id)
         navigate(link)
         setChildId(id)
     }
 
     return (
-        <div>
-            <div className={props.data.children ? 'tabHaveChild' : `tab ${props.id === parentId ? 'active' : ''}`} id={props.id} onClick={(e) => clickHandler(e, props.data)}>
+        <>
+            <div className={props.data.children === null ? ('tab'+ (props.id === parentId ? 'active' : '')) :'tabHaveChild' } id={props.id} onClick={(e) => clickHandler(e, props.data)}>
                 <div className="tabIconText">
                     <div className='icon'>{icons[props.data.icon]}</div>
                     <div className='tabText'>{props.data.name}</div>
                 </div>
-                {props.data.children ?
-                    <div className="dropdownicon"><HiChevronUp /></div>
-                    : ''
+                {props.data.children === null ? '' :
+                    <div className={`dropdownicon ${click === true ? 'active' : ''}`}><HiChevronUp /></div>
                 }
             </div>
 
             <div className={child === true ? 'activechild' : 'child'}>
                 {props.data.children?.map((childItem, key) => (
-                    <div key={key} className={`childItem ${childItem.id === childId ? 'active' : ''}`} id={childItem.id} onClick={(e) => clickChildHandler(e, childItem.id, childItem.link)}>
-                        <div className='icon'>{icons[childItem.icon]}</div>
+                    <div key={key} className={`childItem ${childItem.id === childId ? 'active' : ''}`} id={childItem.id} onClick={() => clickChildHandler(childItem.id, childItem.link)}>
+                        <div className={child === true ? '' : 'icon'}><GoDotFill /></div>
                         <div className=''>{childItem.name}</div>
                     </div>
                 ))
                 }
             </div>
-        </div>
+        </>
     );
 }
 
