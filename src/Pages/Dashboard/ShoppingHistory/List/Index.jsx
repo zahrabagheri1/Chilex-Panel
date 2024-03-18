520d3c8370097cf249af3324d167054dd2c4e0a3
zahrabagheri1 <zahrabagheriuk@gmail.com>
2024-03-18 14:32:17 +0330

    Featcher: Filter - Fix: change Ui filter Shopping history

--- src/Pages/Dashboard/ShoppingHistory/List/Index.jsx
+++ src/Pages/Dashboard/ShoppingHistory/List/Index.jsx
@@ -19,12 +19,12 @@ import ButtonActionGray from '../../../../Components/ButtonActionGray/ButtonActi
 
 function Index() {
     const [historyList, setHistoryList] = useState(null);
+    const [cookies, setCookie, removeCookie] = useCookies(['accessToken']);
     const { setLoading } = useContext(LoadingContext)
     const navigate = useNavigate();
-    const [cookies] = useCookies(['accessToken']);
     const { goToLoginPage } = useContext(LoginContext);
     const [resetFlag, setResetFlag] = useState(false);
-    const [filterBox, setFilterBox] = useState(true);
+    const [filterBox, setFilterBox] = useState(false);
     const [filters, setFilters] = useState({
         userId: null,
         gatewayTypes: null,
@@ -51,6 +51,7 @@ function Index() {
 
     const reqFilterShopHistory = () => {
         setLoading(true)
+        setFilterBox(false)
         axios.get(`${API_URL === undefined ? '' : API_URL}/shopping-history/all?${filters.userId === null || filters.userId === undefined ? '' : 'userId=' + filters.userId + '&'}${filters.gatewayTypes === null || filters.gatewayTypes === undefined ? '' : 'gatewayTypes[]=' + filters.gatewayTypes + '&'}${filters.minAmount === null || filters.minAmount === undefined ? '' : 'minAmount=' + filters.minAmount + '&'}${filters.maxAmount === null || filters.maxAmount === undefined ? '' : 'maxAmount=' + filters.maxAmount + '&'}${filters.type === null || filters.type === undefined ? '' : 'type=' + filters.type + '&'}${filters.referenceType === null || filters.referenceType === undefined ? '' : 'referenceType=' + filters.referenceType + '&'}${filters.sortBy === null || filters.sortBy === undefined ? '' : 'sortBy=' + filters.sortBy + '&'}${filters.orderBy === null || filters.orderBy === undefined ? '' : 'orderBy=' + filters.orderBy + '&'}${filters.offset === null || filters.offset === undefined ? '' : 'offset=' + filters.offset + '&'}${filters.limit === null || filters.limit === undefined ? '' : 'limit=' + filters.limit + '&'}${filters.exportUserIds === null || filters.exportUserIds === undefined ? '' : 'exportUserIds=' + filters.exportUserIds}`,
             {
                 headers: {
@@ -65,7 +66,17 @@ function Index() {
                 }
             )
             .catch(
-                err => console.log(err)
+                err => {
+                    if (err.response.data.statusCode === 401 && err.response.data.message === "Unauthorized") {
+                        removeCookie('accessToken', {
+                            expires: 'Thu, 01 Jan 1970 00:00:00 UTC',
+                        })
+                        navigate('/')
+                    } else {
+                        console.log(err)
+
+                    }
+                }
             )
     }
 
@@ -122,9 +133,18 @@ function Index() {
                     </div>
 
                     <div className={`filter row ${filterBox ? 'activeFilter' : ''}`}>
-
                         <div className="col-xl-4 col-lg-4 col-md-6 col-ms-12 col-xs-12">
                             <Input classname={'filerinput'} value={filters.userId} title={'userId'} name={'userId'} type={'text'} placeholder={'userId...'} changeInputValue={updateInputData} />
+                        </div>
+
+                        <div className="col-xl-4 col-lg-4 col-md-6 col-ms-12 col-xs-12">
+                            <SelectOption classnameBox={'filerinput'} value={filters.type} name={'type'} title={'type'} defaultValue={'type'} type={'status'} readOnly={false} changeOptinValue={updateOptionData}
+                                data={[
+                                    { id: 0, status: 'Gem' },
+                                    { id: 1, status: 'Coin' },
+                                    { id: 1, status: 'Item' },
+                                ]}
+                            />
                         </div>
 
                         <div className="col-xl-4 col-lg-4 col-md-6 col-ms-12 col-xs-12">
@@ -136,8 +156,24 @@ function Index() {
                                 ]}
                             />
                         </div>
+
+                        <div className="col-xl-4 col-lg-4 col-md-6 col-ms-12 col-xs-12">
+                            <SelectOption classnameBox={'filerinput'} value={filters.referenceType} title={'referenceType'} name={'referenceType'} defaultValue={'referenceType'} type={'status'} readOnly={false} changeOptinValue={updateOptionData}
+                                data={[
+                                    { id: 0, status: 'bundle' },
+                                    { id: 1, status: 'item' },
+                                    { id: 1, status: 'transaction' },
+                                    { id: 1, status: 'setting' }
+                                ]}
+                            />
+                        </div>
+
                         <div className="col-xl-4 col-lg-4 col-md-6 col-ms-12 col-xs-12">
                             <Input classname={'filerinput'} value={filters.maxAmount} title={'maxAmount'} name={'maxAmount'} type={'text'} placeholder={'maxAmount...'} changeInputValue={updateInputData} />
+                        </div>
+
+                        <div className="col-xl-4 col-lg-4 col-md-6 col-ms-12 col-xs-12">
+                            <Input classname={'filerinput'} value={filters.minAmount} title={'minAmount'} name={'minAmount'} type={'text'} placeholder={'minAmount...'} changeInputValue={updateInputData} />
                         </div>
 
                         <div className="col-xl-4 col-lg-4 col-md-6 col-ms-12 col-xs-12">
@@ -147,10 +183,6 @@ function Index() {
                                     { id: 1, status: 'ASC' },
                                 ]}
                             />
-                        </div>
-
-                        <div className="col-xl-4 col-lg-4 col-md-6 col-ms-12 col-xs-12">
-                            <Input classname={'filerinput'} value={filters.minAmount} title={'minAmount'} name={'minAmount'} type={'text'} placeholder={'minAmount...'} changeInputValue={updateInputData} />
                         </div>
 
                         <div className="col-xl-4 col-lg-4 col-md-6 col-ms-12 col-xs-12">
@@ -167,28 +199,7 @@ function Index() {
                         </div>
 
                         <div className="col-xl-4 col-lg-4 col-md-6 col-ms-12 col-xs-12">
-                            <SelectOption classnameBox={'filerinput'} value={filters.referenceType} name={'referenceType'} defaultValue={'referenceType'} type={'status'} readOnly={false} changeOptinValue={updateOptionData}
-                                data={[
-                                    { id: 0, status: 'bundle' },
-                                    { id: 1, status: 'item' },
-                                    { id: 1, status: 'transaction' },
-                                    { id: 1, status: 'setting' }
-                                ]}
-                            />
-                        </div>
-
-                        <div className="col-xl-4 col-lg-4 col-md-6 col-ms-12 col-xs-12">
-                            <SelectOption classnameBox={'filerinput'} value={filters.type} name={'type'} defaultValue={'type'} type={'status'} readOnly={false} changeOptinValue={updateOptionData}
-                                data={[
-                                    { id: 0, status: 'Gem' },
-                                    { id: 1, status: 'Coin' },
-                                    { id: 1, status: 'Item' },
-                                ]}
-                            />
-                        </div>
-
-                        <div className="col-xl-4 col-lg-4 col-md-6 col-ms-12 col-xs-12">
-                            <SelectOption classnameBox={'filerinput'} readOnly={false} value={filters.limit} name={'limit'} defaultValue={'15'} type={'status'} changeOptinValue={updateOptionDataForLimit}
+                            <SelectOption classnameBox={'filerinput'} readOnly={false} value={filters.limit} title={'limit'} name={'limit'} defaultValue={'15'} type={'status'} changeOptinValue={updateOptionDataForLimit}
                                 data={[
                                     { id: 20, status: 20 },
                                     { id: 30, status: 30 },
@@ -199,16 +210,11 @@ function Index() {
                             />
                         </div>
 
-                        <ButtonActionBlue title={'Filter'} classnameBtn={'filterBtnBox'} handler={reqFilterShopHistory} />
-                        <Button title={'Reset Filter'} classnameBtn={'filterBtnBox'} btnhandler={resetFillters} />
-                    </div>
-
-                    {/* <div className='filters'>
-
-                        <div className="resetFillters" onClick={resetFillters}>
-                            <HiOutlineTrash />
+                        <div className="filterResetBtn col-xl-12 col-lg-12 col-md-12 col-ms-12 col-xs-12">
+                            <ButtonActionBlue title={'Filter'} classnameBtn={'filterBtnBox'} handler={reqFilterShopHistory} />
+                            <ButtonActionGray title={'Reset Filter'} classnameBtn={'filterBtnBox'} handler={resetFillters} />
                         </div>
-                    </div> */}
+                    </div>
                 </div>
 
                 <div className='exportBtn' onClick={exportUsers}>
