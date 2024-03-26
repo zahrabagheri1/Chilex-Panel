e960f93b7c744d55b4acd807d1e7ed179565d94b
zahrabagheri1 <zahrabagheriuk@gmail.com>
2024-03-26 14:38:24 +0330

    Feature: Charts - Fix: Ludo  Chart Fixed X and Y

--- src/Pages/Dashboard/Charts/LudoChart/Index.jsx
+++ src/Pages/Dashboard/Charts/LudoChart/Index.jsx
@@ -24,6 +24,17 @@ function Index() {
         type: 3
     })
 
+    const dataOFChartX = []
+    const dataOFChartY = []
+
+    ludo?.map(item => (
+        dataOFChartX.push(item.x)
+    ))
+    ludo?.map(item => (
+        dataOFChartY.push(item.y)
+    ))
+
+
     const updateOptionData = (name, id) => {
         setFilters(prev => ({ ...prev, [name]: id }))
     }
@@ -53,15 +64,15 @@ function Index() {
             ).catch(
                 err => {
                     if (err.response.data.statusCode === 401 && err.response.data.message === "Unauthorized") {
-                      removeCookie('accessToken', {
-                        expires: 'Thu, 01 Jan 1970 00:00:00 UTC',
-                      })
-                      navigate('/')
+                        removeCookie('accessToken', {
+                            expires: 'Thu, 01 Jan 1970 00:00:00 UTC',
+                        })
+                        navigate('/')
                     } else {
-                      console.log(err)
-          
+                        console.log(err)
+
                     }
-                  }
+                }
             )
     }
 
@@ -69,7 +80,7 @@ function Index() {
         series: [
             {
                 name: 'ludo',
-                data: ludo
+                data: dataOFChartY
             },
         ],
         options: {
@@ -115,6 +126,9 @@ function Index() {
                     shadeTo: 'dark',
                     shadeIntensity: 1
                 },
+            },
+            xaxis: {
+                categories: dataOFChartX,
             }
         },
     };
@@ -124,7 +138,8 @@ function Index() {
             <div className="chart-filter-box row">
                 <div className="chart-filter-title col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">Ludo</div>
                 <div className="chart-box col-xl-9 col-lg-9 col-md-9 col-sm-9 col-xs-9">
-                    <ReactApexChart options={chartDataOption.options} series={chartDataOption.series} type="area" />
+                    <ReactApexChart options={chartDataOption.options} series={chartDataOption.series} type="line" height={250} width={350} />
+
                 </div>
 
                 <div className="filter-chart col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
