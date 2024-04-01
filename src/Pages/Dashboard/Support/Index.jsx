d23bbc828b574b7f56b67ac5437bf04231682c97
zahrabagheri1 <zahrabagheriuk@gmail.com>
2024-04-02 01:47:52 +0330

    change socket

--- src/Pages/Dashboard/Support/Index.jsx
+++ src/Pages/Dashboard/Support/Index.jsx
@@ -1,113 +1,98 @@
-import React, { useContext, useState } from 'react';
-import './Support.scss';
-import Conversation from '../../../Components/Conversation/Conversation';
-import { useEffect } from 'react';
-import { connectSocketWithToken, socket } from '../../../Socket';
-import Chatroom from './Layers/Chatroom';
-import { LoadingContext } from '../../Loading/LoadingContext';
-import { LoginContext } from '../../Login/LoginContext';
-import { useCookies } from 'react-cookie';
+import React, { useContext, useState } from "react";
+import "./Support.scss";
+import Conversation from "../../../Components/Conversation/Conversation";
+import { useEffect } from "react";
+import { connectSocketWithToken, socket } from "../../../Socket";
+import Chatroom from "./Layers/Chatroom";
+import { LoadingContext } from "../../Loading/LoadingContext";
+import { LoginContext } from "../../Login/LoginContext";
+import { useCookies } from "react-cookie";
 
 function Index() {
-  const [listChats, setListChats] = useState(null)
-  const [idChat, setIdChat] = useState({ status: false, userId: null, image: null, username: null })
+  const [listChats, setListChats] = useState(null);
+  const [idChat, setIdChat] = useState({
+    status: false,
+    userId: null,
+    image: null,
+    username: null,
+  });
   const { setLoading } = useContext(LoadingContext);
   const { goToLoginPage } = useContext(LoginContext);
 
-  // const [isConnected, setIsConnected] = useState(socket.connected);
-  // const [emptyChat, setEmptyChat] = useState(false)
-  // const [newMessage, setNewMessage] = useState('')
-  // const [dataAccount, setDataAccount] = useState({
-  //   pv: null,
-  //   img: null,
-  //   name: null,
-  //   status: null,
-  //   lastseen: null,
-  //   chat: null
-  // })
-  // const [cookies, setCookie] = useCookies(["accessToken"]);
-  // const gotopvuser = (key, value) => {
-  //   setDataAccount(prev => ({ ...prev, [key]: value }))
-  // }
+  const [cookie] = useCookies(["accessToken"]);
 
-  const [cookie] = useCookies(['accessToken']);
-  const searchUser = (e) => {
-    console.log('searchUser')
-  }
 
   useEffect(() => {
-    console.log(" USE EFF",cookie.accessToken)
+    console.log(" USE EFF", cookie.accessToken);
     socket.on("connect", () => {
-      console.table('Connected in support');
+      console.table("Connected in support");
       GetResiveAllChats();
     });
 
-    if (!socket.active)
-      connectSocketWithToken(cookie.accessToken)
+    if (!socket.active) connectSocketWithToken(cookie.accessToken);
     GetResiveAllChats();
 
     socket.on("disconnect", () => {
-      console.table('Disconnected');
+      console.table("Disconnected");
     });
-
   }, []);
 
-  console.log(listChats)
+  const searchUser = (e) => {
+    console.log("searchUser")
+  }
+
+  console.log(listChats);
   function GetResiveAllChats() {
     setLoading(true);
-    console.log("EMIT GET ALL SUPPORT CHATS",socket)
-    socket.emit('adminMessage', 'get-all-support-chats', (response) => {
-      console.log("res",response)
+    console.log("EMIT GET ALL SUPPORT CHATS", socket);
+    socket.emit("adminMessage", "get-all-support-chats", (response) => {
+      console.log("res", response);
       setLoading(false);
-      setListChats(response)
+      setListChats(response);
     });
   }
 
   const showChat = (id, img, username) => {
-    setIdChat({ status: true, userId: id, image: img, username: username })
-  }
+    setIdChat({ status: true, userId: id, image: img, username: username });
+  };
 
   // if (listChats === null || listChats === undefined) {
   //   console.log('Not Load')
   // } else {
-    return (
-      <div className='support'>
-        <div className="chatMenu">
-          <div className="chatMenuWrapper">
-            <input type="search" className='chatMenuSearchBox' placeholder='Search User' onChange={searchUser} />
-            <div className="chatMenuBox">
-              <div className="conversations">
-                {listChats?.map((chat, index) => (
-                  <div key={index}>
-                    <Conversation data={chat} click={showChat} />
-                  </div>
-                ))}
-              </div>
+  return (
+    <div className="support">
+      <div className="chatMenu">
+        <div className="chatMenuWrapper">
+          <input
+            type="search"
+            className="chatMenuSearchBox"
+            placeholder="Search User"
+            onChange={searchUser}
+          />
+          <div className="chatMenuBox">
+            <div className="conversations">
+              {listChats?.map((chat, index) => (
+                <div key={index}>
+                  <Conversation data={chat} click={showChat} />
+                </div>
+              ))}
             </div>
           </div>
         </div>
-        {
-          idChat.status ?
-            <Chatroom id={idChat.userId} data={idChat} limit={15} />
-            :
-            <div className="clearChatRoom">
-              <div className="clearChatRoomtext">
-                Click on One of the Users
-              </div>
-              <div className="clearChatRoomsubtext">
-                they waiting you to answer them!
-              </div>
-            </div>
-        }
       </div>
-    );
-  }
+      {idChat.status ? (
+        <Chatroom id={idChat.userId} data={idChat} limit={15} />
+      ) : (
+        <div className="clearChatRoom">
+          <div className="clearChatRoomtext">Click on One of the Users</div>
+          <div className="clearChatRoomsubtext">
+            they waiting you to answer them!
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
 // }
 
-
 export default Index;
-
-
-
-
-
--- src/Pages/Dashboard/Support/Layers/Chatroom.jsx
+++ src/Pages/Dashboard/Support/Layers/Chatroom.jsx
@@ -1,177 +1,198 @@
-
-import React, {  useRef } from "react";
+import React, { useRef } from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import ButtonActionBlue from "../../../../Components/ButtonActionBlue/ButtonActionBlue";
-import Messege from '../../../../Components/Messege/Messege';
+import Messege from "../../../../Components/Messege/Messege";
 import { socket } from "../../../../Socket";
-import user from '../../../../Assets/image/user.jpg';
+import user from "../../../../Assets/image/user.jpg";
 
 function Chatroom(props) {
-    const inputMsg = useRef()
-    const [chat, setChats] = useState([])
-    const scrollEnd = useRef(null)
-    const [limit, setLimit] = useState(props.limit)
-    const [saveMessages, setSaveMessage] = useState([])
-    const [userId, setUserId] = useState(null)
+  const inputMsg = useRef();
+  const [chat, setChats] = useState([]);
+  const scrollEnd = useRef(null);
+  const [limit, setLimit] = useState(props.limit);
+  const [saveMessages, setSaveMessage] = useState([]);
+  const [userId, setUserId] = useState(null);
 
-    socket.off("chat")
-    socket.on("chat", (data) => {
-        if (data.senderId == userId) {
-            let temp = JSON.parse(JSON.stringify(chat))
-            temp.reverse()
-            temp.push(data)
-            temp.reverse()
-            setChats(temp)
-        }
-    })
+  socket.off("chat");
+  socket.on("chat", (data) => {
+    if (data.senderId == userId) {
+      let temp = JSON.parse(JSON.stringify(chat));
+      temp.reverse();
+      temp.push(data);
+      temp.reverse();
+      setChats(temp);
+    }
+  });
 
-    useEffect(() => {
-        ResiveChts(props.id, limit);
-        setUserId(props.id)
-        if (scrollEnd.current) {
-          scrollEnd.current.addEventListener('scroll', handleScroll);
-        }
-        return () => {
-            if (scrollEnd.current) {
-                scrollEnd.current.removeEventListener('scroll', handleScroll);
-            }
-        };
-    }, [props.id, limit]);
-
-    const handleScroll = (counter) => {
-        if (counter === 15) {
-            if (scrollEnd.current.scrollTop === 0) {
-                scrollEnd.current.scrollTop = scrollEnd.current.scrollHeight
-                console.log('limit xaz :', limit)
-            }
-        } else {
-            if (scrollEnd.current.scrollTop === 0) {
-                // setLimit(counter + 15)
-                // console.log('limit  :', limit)
-                // ResiveChts(props.id, limit)
-            }
-        }
-
-        //     console.log('handleScroll : ',scrollEnd.current.scrollHeight)
-        //     if (counterChats === 20) {
-        //         return ''
-        //     } else {
-        //         if (scrollEnd.current.scrollTop === 0) {
-        //             setCounterChats(counterChats + 20)
-        //             ResiveChts(props.id)
-        //             // scrollEnd.current.scrollTop = scrollEnd.current.scrollHeight
-        //             // console.log('counter sended :' , counterChats)
-        //         } else if (scrollEnd.current.scrollTop === scrollEnd.current.scrollHeight) {
-        //             setCounterChats(counterChats - 20)
-        //             ResiveChts(props.id)    
-        //         } else {
-        //             console.log(counterChats, scrollEnd.current.scrollTop)
-        //         }
-        //     }
-
+  useEffect(() => {
+    ResiveChts(props.id, limit);
+    setUserId(props.id);
+    if (scrollEnd.current) {
+      scrollEnd.current.addEventListener("scroll", handleScroll);
+    }
+    return () => {
+      if (scrollEnd.current) {
+        scrollEnd.current.removeEventListener("scroll", handleScroll);
+      }
     };
+  }, [props.id, limit]);
 
-    const ResiveChts = (id, counter) => {
-        const data = { anotherPersonId: id, limit: counter, offset: 0 }
-        socket.emit('adminMessage', 'get-a-support-chat', data, (response) => {
-            if (userId !== props.id) {
-                setLimit(15);
-            } 
-            setChats(response.chatBoxes);
-            handleScroll(counter);
-        });
-    };
-
-    const ReadMoreMessages = () => {
-        if (chat.length >= limit) {
-            setLimit(limit + 15)
-            ResiveChts(props.id, limit)
-        }
+  const handleScroll = (counter) => {
+    if (counter === 15) {
+      if (scrollEnd.current.scrollTop === 0) {
+        scrollEnd.current.scrollTop = scrollEnd.current.scrollHeight;
+        console.log("limit xaz :", limit);
+      }
+    } else {
+      if (scrollEnd.current.scrollTop === 0) {
+        // setLimit(counter + 15)
+        // console.log('limit  :', limit)
+        // ResiveChts(props.id, limit)
+      }
     }
 
-    const SendMessage = () => {
-        socket.emit('adminMessage', 'chat', {
-            message: inputMsg.current.value,
-            anotherPersonId: props.id,
-            messageType: 0
-        }, (response) => {
-            ResiveChts(props.id, 0)
-            inputMsg.current.value = ''
-        });
+    //     console.log('handleScroll : ',scrollEnd.current.scrollHeight)
+    //     if (counterChats === 20) {
+    //         return ''
+    //     } else {
+    //         if (scrollEnd.current.scrollTop === 0) {
+    //             setCounterChats(counterChats + 20)
+    //             ResiveChts(props.id)
+    //             // scrollEnd.current.scrollTop = scrollEnd.current.scrollHeight
+    //             // console.log('counter sended :' , counterChats)
+    //         } else if (scrollEnd.current.scrollTop === scrollEnd.current.scrollHeight) {
+    //             setCounterChats(counterChats - 20)
+    //             ResiveChts(props.id)
+    //         } else {
+    //             console.log(counterChats, scrollEnd.current.scrollTop)
+    //         }
+    //     }
+  };
+
+  const ResiveChts = (id, counter) => {
+    const data = { anotherPersonId: id, limit: counter, offset: 0 };
+    socket.emit("adminMessage", "get-a-support-chat", data, (response) => {
+      if (userId !== props.id) {
+        setLimit(15);
+      }
+      setChats(response.chatBoxes);
+      handleScroll(counter);
+    });
+  };
+
+  const ReadMoreMessages = () => {
+    if (chat.length >= limit) {
+      setLimit(limit + 15);
+      ResiveChts(props.id, limit);
     }
+  };
 
-    const handleKeyPress = (event) => {
-        if (event.key === 'Enter') {
-            event.preventDefault()
-            SendMessage()
-        }
+  const SendMessage = () => {
+    socket.emit(
+      "adminMessage",
+      "chat",
+      {
+        message: inputMsg.current.value,
+        anotherPersonId: props.id,
+        messageType: 0,
+      },
+      (response) => {
+        ResiveChts(props.id, 0);
+        inputMsg.current.value = "";
+      }
+    );
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      SendMessage();
     }
+  };
 
-    return (
-        <div className="chatBox">
-            <div className="chatBoxWrapper">
-                <div className="chatBoxMain">
-                    <div className="chatboxheader">
-                        <div className='chatHeader'>
-                            <img className='chatHeaderImg' src={props.data.image || user} alt="" />
-                            <div className="chatHeaderName">
-                                <div className="chatHeaderusername">{props.data.username || chat?.userName}</div>
-                                <div className="chatHeaderactivity">{props.data.status || chat?.status ? 'Online' : ''}</div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div ref={scrollEnd} className="chatboxbody">
-                        {
-                            chat.length >= limit ?
-                                <button className="readmoremessages" onClick={ReadMoreMessages}>
-                                    <div className="textlineright"></div>
-                                    <div className="readmore">read more messages</div>
-                                    <div className="textlinelift"></div>
-                                </button>
-                                : ''
-                        }
-
-                        {chat?.map((msg, index) => (
-                            <div key={index}>
-                                <Messege message={msg.message} name={chat?.userName} receiverId={msg?.receiverId} own={msg?.senderId} time={msg?.date} />
-                            </div>
-                        )).reverse()
-                        }
-
-                        {saveMessages?.map((msg, index) => (
-                            <div key={index}>
-                                <Messege message={msg.message} name={chat?.userName} receiverId={msg?.receiverId} own={msg?.senderId} time={msg?.date} />
-                            </div>
-                        )).reverse()}
-
-                    </div>
+  return (
+    <div className="chatBox">
+      <div className="chatBoxWrapper">
+        <div className="chatBoxMain">
+          <div className="chatboxheader">
+            <div className="chatHeader">
+              <img
+                className="chatHeaderImg"
+                src={props.data.image || user}
+                alt=""
+              />
+              <div className="chatHeaderName">
+                <div className="chatHeaderusername">
+                  {props.data.username || chat?.userName}
                 </div>
-
-
-                <div className="sendtextBox">
-
-                    <div className="chatBoxTextarea">
-                        <textarea
-                            className="chatMessageInput"
-                            placeholder='write somthing...'
-                            ref={inputMsg}
-                            onKeyDown={handleKeyPress}
-                        >
-                        </textarea>
-
-                        <ButtonActionBlue
-                            title={'Send'}
-                            className={'chatSubmitButton'}
-                            handler={SendMessage}
-                        />
-                    </div>
+                <div className="chatHeaderactivity">
+                  {props.data.status || chat?.status ? "Online" : ""}
                 </div>
+              </div>
             </div>
+          </div>
+
+          <div ref={scrollEnd} className="chatboxbody">
+            {chat.length >= limit ? (
+              <button className="readmoremessages" onClick={ReadMoreMessages}>
+                <div className="textlineright"></div>
+                <div className="readmore">read more messages</div>
+                <div className="textlinelift"></div>
+              </button>
+            ) : (
+              ""
+            )}
+
+            {chat
+              ?.map((msg, index) => (
+                <div key={index}>
+                  <Messege
+                    message={msg.message}
+                    name={chat?.userName}
+                    receiverId={msg?.receiverId}
+                    own={msg?.senderId}
+                    time={msg?.date}
+                  />
+                </div>
+              ))
+              .reverse()}
+
+            {saveMessages
+              ?.map((msg, index) => (
+                <div key={index}>
+                  <Messege
+                    message={msg.message}
+                    name={chat?.userName}
+                    receiverId={msg?.receiverId}
+                    own={msg?.senderId}
+                    time={msg?.date}
+                  />
+                </div>
+              ))
+              .reverse()}
+          </div>
         </div>
 
-    )
+        <div className="sendtextBox">
+          <div className="chatBoxTextarea">
+            <textarea
+              className="chatMessageInput"
+              placeholder="write somthing..."
+              ref={inputMsg}
+              onKeyDown={handleKeyPress}
+            ></textarea>
+
+            <ButtonActionBlue
+              title={"Send"}
+              className={"chatSubmitButton"}
+              handler={SendMessage}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Chatroom;
